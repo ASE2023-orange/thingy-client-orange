1b2430ca93e0dc3a3e1ac212047b2cf62a560138
Jean-Marie Alder <jean-marie.alder@hevs.ch>
2023-10-15 21:28:08 +0200

    working login and simple api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'thingy-client-orange';
+  message: string;
+
+  constructor(private http: HttpClient) {
+    this.message = ''; // Initialize the message variable
+  }
+
+  ngOnInit() {
+    this.fetchMessageFromAPI();
+  }
+
+  fetchMessageFromAPI() {
+    // this.http.get<any>('http://localhost:8080/test')
+    //   .subscribe(
+    //     (response) => {
+    //       // Assuming the API response has a "message" property
+    //       this.message = response.message;
+    //     },
+    //     (error) => {
+    //       console.error('Error fetching data:', error);
+    //     }
+    //   );
+    return this.http.get('/api/test', {responseType:'json'})
+      .subscribe((data: any) => {
+        console.log(data['message'])
+        this.message = data['message']
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,32 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { KeycloakAngularModule, KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+/**
+ * Initializes and returns keycloak configuration
+ * @param keycloak KeycloakService
+ * @returns Keycloak configuration
+ */
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8888/',
+        realm: 'thingy-orange',
+        clientId: 'frontend'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      enableBearerInterceptor: true,
+    });
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +34,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
