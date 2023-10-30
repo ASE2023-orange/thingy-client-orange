447fa4757bce3b7fd461dec6add126c0d0c15263
Leyla Kande <fatou.leyla.kande@gmail.com>
2023-10-30 18:07:49 +0100

    periodic get of latest data points

--- src/app/realtime-data/realtime-data.component.ts
+++ src/app/realtime-data/realtime-data.component.ts
@@ -1,61 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-realtime-data',
   templateUrl: './realtime-data.component.html',
   styleUrls: ['./realtime-data.component.css']
 })
-export class RealtimeDataComponent implements OnInit, OnDestroy {
+export class RealtimeDataComponent implements OnInit, OnDestroy{
   sensorData = new Map<string, string>()
 
-  thingyData: any;
-  airPressure: any;
+  pressure: any;
+  humidity: any;
+  light: any;
 
-
-  private eventSource = new EventSource('http://localhost:8080/live_data', { withCredentials: true });
-  success = "success";
+  subscription: Subscription = new Subscription;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     console.log("start");
     this.fetchThingyData();
-
-    this.eventSource.onmessage = (event) => {
-      console.log("sse update");
-      this.success = event.data;
-    };
-
-    this.eventSource.onerror = (error) => {
-      console.error('SSE error: ', error);
-    };
+    this.subscription = interval(3000).subscribe(() => {
+      this.fetchThingyData();
+    })
   }
 
   ngOnDestroy(): void {
-    this.eventSource.close();
+    this.subscription.unsubscribe();
   }
 
   fetchThingyData() {
     return this.http.get('/api/thingy', {responseType:'json'})
     .subscribe((data: any) => {
-      this.thingyData = data;
-      // console.log('thingy data: receives', this.thingyData);
-
-      // const jsonData = JSON.parse(data);
-      // if (jsonData.appId === 'AIR_PRESS') {
-      //   // this.sensorData.set('airPressure', jsonData.data);
-      //   this.airPressure = jsonData.data;
-      // } else if (jsonData.appId === 'HUMID'){
-      //   this.sensorData.set('humidity', jsonData.data);
-      // } else if (jsonData.appId === 'LIGHT') {
-      //   this.sensorData.set('light', jsonData.data);
-      // }
+      this.pressure = data.pressure;
+      this.humidity = data.humidity;
+      this.light = data.light;
+      console.log("retrieve thingy data")
     });
-  }
-
-  getSSEData() {
-    this
   }
 
 }
